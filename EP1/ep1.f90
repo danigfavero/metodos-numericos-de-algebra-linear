71c1d8476f8849b69f4ccc6e0768df3b84042860
danigfavero <danifavero@usp.br>
2020-09-03 00:11:07 -0300

    pelo menos compila kkkk

--- EP1/ep1.f90
+++ EP1/ep1.f90
@@ -2,6 +2,8 @@
 ! NUSP: 10277443
 ! Exercício Programa 1 - Produto interno, norma euclidiana, e produto envolvendo matrizes e vetores
 
+
+! Recebe um inteiro n e dois vetores x, y ∈ R n . Devolve o produto interno entre x e y.
 function euclidean_norm(n, x) result(norm)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
@@ -13,6 +15,8 @@ function euclidean_norm(n, x) result(norm)
   
 end function euclidean_norm
 
+
+! Recebe um inteiro n e um vetor x ∈ R n . Devolve a norma eucluidiana do vetor x.
 function dotproduct(n, x, y) result(product)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
@@ -30,12 +34,12 @@ program ep1
     implicit none
 
     ! TESTING
-    integer :: n
-    n = 10
-    real(dp) :: array1(n) 
-    real(dp) :: array2(n) 
-    array1 = [(i,i=1,10)]
-    array1(:) = 0 
-    print *, dot_product(n, array1, array2)
+    ! integer :: n
+    ! n = 10
+    ! real(dp) :: array1(n) 
+    ! real(dp) :: array2(n) 
+    ! array1 = [(i,i=1,10)]
+    ! array1(:) = 0 
+    ! print *, dot_product(n, array1, array2)
 
 end program ep1
