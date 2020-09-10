e4bc1bae89f95523df7b4601db4574b1af3b83a1
danigfavero <danifavero@usp.br>
2020-09-10 15:08:19 -0300

    norma funciona

--- EP1/ep1.f90
+++ EP1/ep1.f90
@@ -2,21 +2,27 @@
 ! NUSP: 10277443
 ! Exercício Programa 1 - Produto interno, norma euclidiana, e produto envolvendo matrizes e vetores
 
-
-! Recebe um inteiro n e dois vetores x, y ∈ R n . Devolve o produto interno entre x e y.
-function euclidean_norm(n, x) result(norm)
+function naive_euclidean_norm(n, x) result(norm)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
     integer, intent(in) :: n
     real(dp), intent(in) :: x(n)
-    real :: norm
+    real(dp) :: y(n)
+    real(dp) :: norm
+    integer :: i
+
+    do i=1,n
+        y(i) = x(i)**2
+    enddo
+    norm = dsqrt(sum(y))
   
-    norm = sqrt(sum(x**2))
-  
-end function euclidean_norm
+end function naive_euclidean_norm
 
 
-! Recebe um inteiro n e um vetor x ∈ R n . Devolve a norma eucluidiana do vetor x.
+!-----------------------------------------------------------------------------------------------
+
+
+! Recebe um inteiro n e dois vetores x, y ∈ R n . Devolve o produto interno entre x e y.
 function dotproduct(n, x, y) result(product)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
@@ -28,6 +34,29 @@ function dotproduct(n, x, y) result(product)
     product = 3
 
 end function dotproduct
+
+
+! Recebe um inteiro n e um vetor de tamanho n. Devolve a norma eucluidiana do vetor x.
+function euclidean_norm(n, x) result(norm)
+    use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
+    implicit none
+    integer, intent(in) :: n
+    real(dp), intent(in) :: x(n)
+    real(dp) :: y(n)
+    real(dp) :: norm
+    real(dp) :: max
+    integer :: i
+    
+    max = maxval(x)
+    do i=1,n
+        y(i) = x(i) / max
+        y(i) = y(i)**2
+        
+    enddo
+
+    norm = dsqrt(sum(y)) * 3
+  
+end function euclidean_norm
 
 
 ! Recebe dois inteiros n, m, uma matriz A ∈ R n×m , e dois vetores x ∈ R m , b ∈ Rn. A função
@@ -69,13 +98,13 @@ program ep1
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
 
-    ! TESTING
-    ! integer :: n
-    ! n = 10
-    ! real(dp) :: array1(n) 
-    ! real(dp) :: array2(n) 
-    ! array1 = [(i,i=1,10)]
-    ! array1(:) = 0 
-    ! print *, dot_product(n, array1, array2)
+    ! TESTE NORMA
+    real(dp) :: a(3)
+    real(dp) :: naive_euclidean_norm
+    real(dp) :: euclidean_norm
+    a = [1.0, 2.0, 3.0]
+    print *, "Norma inocente: ", naive_euclidean_norm(3, a)
+    print *, "Norma: ", euclidean_norm(3, a)
+    print *, "Norma built-in: ", norm2(a)
 
 end program ep1
