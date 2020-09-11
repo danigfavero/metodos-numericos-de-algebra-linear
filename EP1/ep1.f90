21c86eec12773931fb0683de01647ffb0129237a
danigfavero <danifavero@usp.br>
2020-09-10 21:10:40 -0300

    produto matriz vetor funciona

--- EP1/ep1.f90
+++ EP1/ep1.f90
@@ -1,9 +1,11 @@
 ! NOME: Daniela Gonzalez Favero
 ! NUSP: 10277443
-! Exercício Programa 1 - Produto interno, norma euclidiana, e produto envolvendo matrizes e vetores
+! Exercício Programa 1 - Produto interno, norma euclidiana, e produto 
+! envolvendo matrizes e vetores
 
 
-! Recebe um inteiro n e dois vetores x, y ∈ R n . Devolve o produto interno entre x e y.
+! Recebe um inteiro n e dois vetores x, y de tamanho n. Devolve o produto
+! interno entre x e y.
 function dotproduct(n, x, y) result(product)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
@@ -24,7 +26,8 @@ function dotproduct(n, x, y) result(product)
 end function dotproduct
 
 
-! Recebe um inteiro n e um vetor de tamanho n. Devolve a norma eucluidiana do vetor x.
+! Recebe um inteiro n e um vetor de tamanho n. Devolve a norma euclidiana do
+! vetor x.
 function euclidean_norm(n, x) result(norm)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
@@ -47,8 +50,9 @@ function euclidean_norm(n, x) result(norm)
 end function euclidean_norm
 
 
-! Recebe dois inteiros n, m, uma matriz A ∈ R n×m , e dois vetores x ∈ R m , b ∈ Rn. A função
-! devolve, no vetor b, o resultado do produto matriz por vetor entre A e x, ou seja, b = Ax.
+! Recebe dois inteiros n, m, uma matriz A (nXm) e dois vetores x de tamanho m,
+! b de tamanho n. A função devolve, no vetor b, o resultado do produto matriz
+! por vetor entre A e x, ou seja, b = Ax.
 subroutine matrixvector(n, m, A, x, b)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
@@ -56,19 +60,22 @@ subroutine matrixvector(n, m, A, x, b)
     integer, intent(in) :: m
     real(dp), intent(in) :: A(n,m)
     real(dp), intent(in) :: x(m)
-    real(dp), intent(in) :: b(n)
+    real(dp) :: b(n)
 
     integer :: i
+    integer :: j
     do i=1,n
-      print *,A(i,1:m)
-    end do
-
+        do j=1,m
+            b(j) = b(j) + A(j,i) * x(i)
+        enddo
+    enddo
 
 end subroutine matrixvector
 
 
-! Recebe três inteiros n, m, p e três matrizes A ∈ R n×m , X ∈ R m×p , B ∈ R n×p . A função
-! devolve, na matriz B, o resultado do produto matriz por matriz entre A e X, ou seja, B = AX.
+! Recebe três inteiros n, m, p e três matrizes A (nXm) , X (mXp) e B (nXp). A
+! função devolve, na matriz B, o resultado do produto matriz por matriz entre A
+! e X, ou seja, B = AX.
 subroutine matrixmatrix(n, m, p, A, X, B)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
@@ -77,11 +84,11 @@ subroutine matrixmatrix(n, m, p, A, X, B)
     integer, intent(in) :: p
     real(dp), intent(in) :: A(n,m)
     real(dp), intent(in) :: X(m,p)
-    real(dp), intent(in) :: B(n,p)
+    real(dp) :: B(n,p)
 
 end subroutine matrixmatrix
 
-!-----------------------------------------------------------------------------------------------
+! -----------------------------------------------------------------------------
 
 function naive_euclidean_norm(n, x) result(norm)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
@@ -99,25 +106,105 @@ function naive_euclidean_norm(n, x) result(norm)
 
 end function naive_euclidean_norm
 
+subroutine naive_matrixvector(n, m, A, x, b)
+    use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
+    implicit none
+    integer, intent(in) :: n
+    integer, intent(in) :: m
+    real(dp), intent(in) :: A(n,m)
+    real(dp), intent(in) :: x(m)
+    real(dp) :: b(n)
+
+    integer :: i
+    integer :: j
+    do i=1,n
+        b(i) = 0.0
+        do j=1,m
+            b(i) = b(i) + A(i,j) * x(j)
+        enddo
+    enddo
+
+end subroutine naive_matrixvector
+
 
 program ep1
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
-    real(dp) :: a(3)
+    real(dp) :: v(3)
     real(dp) :: naive_euclidean_norm
     real(dp) :: euclidean_norm
     real(dp) :: dotproduct
-    a = [1.0, 2.0, 3.0]
+    real(dp), allocatable :: M(:,:)
+    real(dp), allocatable :: q(:)
+    real(dp), allocatable :: u(:)
+    real(dp), allocatable :: N(:,:)
+    real(dp), allocatable :: O(:,:)
+    integer :: i
+    integer :: j
+    integer :: meunumero
+    real(kind=8)::start,finish
 
     ! TESTE NORMA
-    print *, "Norma inocente: ", naive_euclidean_norm(3, a)
-    print *, "Norma: ", euclidean_norm(3, a)
-    print *, "Norma built-in: ", norm2(a)
+    v = [1.0, 2.0, 3.0]
+    print *, "Norma inocente: ", naive_euclidean_norm(3, v)
+    print *, "Norma: ", euclidean_norm(3, v)
+    print *, "Norma built-in: ", norm2(v)
 
     print *, ""
 
     ! TESTE PRODUTO INTERNO
-    print *, "Produto interno: ", dotproduct(3, a, a)
-    print *, "Produto interno built-in: ", dot_product(a, a)
+    print *, "Produto interno: ", dotproduct(3, v, v)
+    print *, "Produto interno built-in: ", dot_product(v, v)
+
+    print *, ""
+
+    ! TESTE MATRIZ VETOR
+    meunumero = 20000
+    allocate(M(meunumero,meunumero))
+    allocate(q(meunumero))
+    allocate(u(meunumero))
+    do i=1,meunumero
+        q(i) = i * 3.1
+        do j=1,meunumero
+            M(i,j) = i+j * 2.7
+        enddo
+    enddo
+
+    call cpu_time(start)
+    call matrixvector(meunumero, meunumero, M, q, u)
+    call cpu_time(finish)
+    print *, "Produto matriz vetor: ", finish-start
+
+    call cpu_time(start)
+    call naive_matrixvector(meunumero, meunumero, M, q, u)
+    call cpu_time(finish)
+    print *, "Produto matriz vetor naive: ", finish-start
+
+    call cpu_time(start)
+    u = matmul(M, q)
+    call cpu_time(finish)
+    print *, "Produto matriz vetor built-in: ", finish-start
+
+    deallocate(M)
+    deallocate(q)
+    deallocate(u)
+
+    print *, ""
+
+    ! TESTE MATRIZ MATRIZ
+    ! meunumero = 20000
+    ! allocate(M(meunumero,meunumero))
+    ! allocate(N(meunumero,meunumero))
+    ! allocate(O(meunumero,meunumero))
+    ! do i=1,meunumero
+    !     q(i) = i * 3.1
+    !     do j=1,meunumero
+    !         M(i,j) = i+j * 2.7
+    !     enddo
+    ! enddo
+    ! call matrixmatrix(3, 3, 3, M, N, O)
+    ! print *, "Multiplicação de matrizes: ", O
+    ! print *, "Multiplicação de matrizes built-in: ", matmul(M, N)
+
 
 end program ep1
