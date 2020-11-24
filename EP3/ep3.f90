172b6fb94eb571564aee3bdfa5c0d9280ac11348
danigfavero <danifavero@usp.br>
2020-11-23 22:41:22 -0300

    [EP3] codigo revisado

--- EP3/ep3.f90
+++ EP3/ep3.f90
@@ -2,6 +2,119 @@
 ! NUSP: 10277443
 ! Exercício Programa 3 - Matrizes ortogonais e o problema de quadrados mínimos
 
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FUNÇÕES AUXILIARES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+! Dada uma matriz A de dimensão nXm, a subrotina rescale() reescala toda a
+! matriz para evitar overflow, dividindo todos os elementos pelo maior valor
+! absoluto dentre os elementos. Ela também devolve o valor máximo da matriz em
+! max.
+subroutine rescale(A, n, m, max)
+    use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
+    use, intrinsic :: ieee_arithmetic
+    implicit none
+    integer, intent(in) :: n, m
+    real(dp) :: max, A(n,m)
+    integer :: i, j
+
+    max = 0
+    do j=1,m
+        do i=1,n 
+            if (abs(A(i,j)) > max) then
+                max = abs(A(i,j))
+            endif
+        enddo
+    enddo
+
+    do j=1,m
+        do i=1,n 
+            A(i,j) = A(i,j)/max
+        enddo
+    enddo
+
+end subroutine rescale
+
+! Recebe uma matriz A de dimensão nXm, um valor de iteração k-1 e um vetor de
+! normas norms contendo as normas das colunas calculadas até a iteração atual.
+! Devolve o vetor norms com as normas da iteração k, além do vetor perm que
+! guarda as permutações das colunas e se o posto (rank) da matriz é completo
+! (1) ou incompleto (0).
+function permutation(A, n, m, k, norms, perm) result(rank)
+    use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
+    use, intrinsic :: ieee_arithmetic
+    implicit none
+    integer, intent(in) :: n, m
+    real(dp) :: max_val, A(n,m), norms(m), eps, aux
+    integer :: k, i, j, max, rank, perm(m)
+
+    rank = 1
+    eps = 0
+
+    if (k > 1) then
+        do j=k,m
+            norms(j) = norms(j) - A(k-1,j)**2
+        enddo
+    endif
+
+    max = k
+    max_val = norms(k)
+    do j=k+1,m
+        if (norms(j) > max_val) then
+            max = j
+            max_val = norms(j)
+        endif
+    enddo
+
+    if (max_val < eps) then
+        rank = 0 ! posto incompleto
+    else
+        perm(k) = max
+
+        if (max /= k) then
+            aux = norms(k)
+            norms(k) = norms(max)
+            norms(max) = aux
+
+            do i=1,n 
+                aux = A(i,k)
+                A(i,k) = A(i, max)
+                A(i, max) = aux
+            enddo
+
+        endif
+
+    endif
+
+end function permutation
+
+! Dada uma matriz A de dimensão nXm, o valor da iteração atual k e o vetor
+! norms com as normas das colunas de A, o algoritmo calcula tau, gamma e u tais
+! que Q=(I-gamma*u*u^T) é um refletor para a coluna k de A. Se a coluna k é
+! zero, gamma = 0, dando Q=I. Caso contrário, gamma e u são produzidos, e u é
+! armazenada na coluna k. Como u(k) é 1, armazenamos -tau em A(k,k).
+function reflector(A, n, m, k, norms) result(gamma)
+    use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
+    use, intrinsic :: ieee_arithmetic
+    implicit none
+    integer :: n, m, k, i
+    real(dp) :: A(n,m), gamma, tau, norms(m)
+
+    tau = dsqrt(norms(k))
+    if (A(k,k) < 0) then
+        tau = -tau
+    endif
+
+    A(k,k) = tau + A(k,k)
+    gamma = A(k,k)/tau
+
+    do i=k+1,n
+        A(i,k) = A(i,k)/A(k,k)
+    enddo
+
+    A(k,k) = -tau
+
+end function reflector
+
 ! Recebe um inteiro n, uma matriz triangular A (nXn), um vetor b (n). A função
 ! devolve, no próprio b, a solução do sistema triangular desejado, 0 se o
 ! sistema foi calculado com sucesso e -1 se a matriz A for singular.
@@ -29,122 +142,14 @@ function backcol(n, A, b) result(sucesso)
 
 end function backcol
 
-! Dada uma matriz A de dimensão nXm, o algoritmo calcula tau, gamma e u tais
-! que Q=(I-gamma*u*u^T) é um refletor para a coluna k de A. Se a coluna k é
-! zero, gamma = 0, dando Q=I. Caso contrário, gamma e u são produzidos, e u é
-! armazenada na coluna k. Como u(k) é 1, armazenamos tau em A(k,k).
-function reflector(A, n, m, k, beta, tau) result(gamma)
-    use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
-    use, intrinsic :: ieee_arithmetic
-    implicit none
-    real(dp) :: A(n,m), beta, gamma, tau
-    integer :: n, m, k, i
+!!!!!!!!!!!!!!!!!!! QR DECOMPOSITION & LEAST SQUARES METHOD !!!!!!!!!!!!!!!!!!!
 
-    beta = 0
-    do i=k,n
-        if (abs(A(i,k)) > beta) then
-            beta = abs(A(i,k))
-        endif
-    enddo
-
-    if (beta == 0) then ! Q = I
-        gamma = 0
-    else
-        tau = 0
-        do i=k,n
-            A(i,k) = A(i,k)/beta
-            tau = tau + A(i,k)**2
-        enddo
-        tau = dsqrt(tau)
-
-        if (A(k,k) < 0) then
-            tau = -tau
-        endif
-
-        A(k,k) = tau + A(k,k)
-        gamma = A(k,k)/tau
-        do i=k+1,n
-            A(i,k) = A(i,k)/A(k,k)
-        enddo
-        tau = tau * beta
-        A(k,k) = tau
-    endif
-
-end function reflector
-
-subroutine rescale(A, n, m)
-    use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
-    use, intrinsic :: ieee_arithmetic
-    implicit none
-    integer, intent(in) :: n, m
-    real(dp) :: max, A(n,m)
-    integer :: i, j
-
-    max = 0
-    do j=1,m
-        do i=1,n 
-            if (abs(A(i,j)) > max) then
-                max = abs(A(i,j))
-            endif
-        enddo
-    enddo
-
-    do j=1,m
-        do i=1,n 
-            A(i,j) = A(i,j)/max
-        enddo
-    enddo
-
-end subroutine rescale
-
-function permutation(A, n, m, k, norms, perm, max_val) result(rank)
-    use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
-    use, intrinsic :: ieee_arithmetic
-    implicit none
-    integer, intent(in) :: n, m
-    real(dp) :: max_val, A(n,m), norms(m), eps, aux
-    integer :: k, i, j, max, rank, perm(m)
-
-    rank = 1
-    eps = 0
-
-    if (k /= 1) then
-        do j=k,m
-            norms(j) = norms(j) - A(k-1,j)**2
-        enddo
-    endif
-
-    max = k
-    max_val = norms(k)
-    do j=k+1,m
-        if (norms(j) > max_val) then
-            max = j
-            max_val = norms(j)
-        endif
-    enddo
-
-    if (max_val < eps) then
-        rank = 0
-    else
-        perm(k) = max
-        if (max /= k) then
-            aux = norms(k)
-            norms(k) = norms(max)
-            norms(max) = aux
-
-            do i=1,n 
-                aux = A(i,k)
-                A(i,k) = A(i, max)
-                A(i, max) = aux
-            enddo
-
-        endif
-
-    endif
-
-end function permutation
-
-! se gamma = 0 => A singular
+! Dada uma matriz A de dimensão nXm, calcula a decomposição da mesma em QR,
+! sendo Q uma matriz ortogonal (armazenada não explicitamente na parte
+! triangular inferior) de A e R uma matriz triangular superior armazenada
+! também em A. A subrotina também devolve o vetor gammas com ,
+! o vetor perm com as permutações realizadas na decomposição, o valor max com o
+! elemento máximo da matriz e o valor rank com o posto de A.
 subroutine qr(A, n, m, gammas, perm, max, rank)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     use, intrinsic :: ieee_arithmetic
@@ -152,10 +157,10 @@ subroutine qr(A, n, m, gammas, perm, max, rank)
     integer, intent(in) :: n, m
     integer :: k, i, j, perm(m), permutation, rank
     real(dp) :: A(n,m), v(m), gammas(m)
-    real(dp) ::tau, beta, norms(m), max
+    real(dp) :: norms(m), max
     real(dp) :: reflector
 
-    call rescale(A, n, m)
+    call rescale(A, n, m, max)
 
     ! calcula as normas de todas as colunas da matriz inicial
     do j=1,m
@@ -164,20 +169,20 @@ subroutine qr(A, n, m, gammas, perm, max, rank)
             norms(j) = norms(j) + A(i,j)**2 
         enddo
     enddo
-    
-    do k=1,m-1
 
-        if (permutation(A, n, m, k, norms, perm, max) == 0) then
+    ! finalmente, a decomposição QR
+    do k=1,m-1
+        if (permutation(A, n, m, k, norms, perm) == 0) then
             exit ! posto incompleto
         endif
 
-        gammas(k) = reflector(A, n, m, k, beta, tau)
+        gammas(k) = reflector(A, n, m, k, norms)
 
         ! v^T <- (gamma * u^T) * B
         do j=k+1,m 
             v(j) = 0
             do i=k+1,n
-                v(j) = v(j) + (gammas(k) * A(i, k)) * A(i,j)
+                v(j) = v(j) + (gammas(k) * A(k, j)) * A(i,j)
             enddo
         enddo
 
@@ -194,7 +199,8 @@ subroutine qr(A, n, m, gammas, perm, max, rank)
 
 end subroutine qr
 
-! resolve
+! Dada uma matriz A de dimensão nXm e um vetor b de dimensão n, a subrotina
+! calcula o sistema superdeterminado Ax=b, devolvendo o vetor x em b.
 subroutine leastsquares(n, m, A, b)
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     use, intrinsic :: ieee_arithmetic
@@ -225,7 +231,7 @@ subroutine leastsquares(n, m, A, b)
 
     enddo
 
-    singular = backcol(m, A, b)
+    singular = backcol(rank, A, b)
 
     do i=1,rank
         aux = b(i)
@@ -235,14 +241,18 @@ subroutine leastsquares(n, m, A, b)
 
 end subroutine leastsquares
 
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FUNÇÃO MAIN !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 program ep3
     use, intrinsic :: iso_c_binding, only: sp=>c_float, dp=>c_double
     implicit none
     integer :: n, m, i, j
-    real(dp), allocatable :: A(:,:)
-    real(dp) :: aij
+    real(dp), allocatable :: A(:,:), b(:)
+    real(dp) :: aij, bi
 
     read (*,*) n, m
+
     allocate(A(n,m))
     do i=1,n
         do j=1,m
@@ -251,5 +261,17 @@ program ep3
         enddo
     enddo
 
+    allocate(b(n))
+    do i=1,n
+        read(*,*) bi
+        b(i) = bi
+    enddo
+
+    call leastsquares(n, m, A, b)
+
+    print *,b
+
+    deallocate(A)
+    deallocate(b)
 
 end program ep3
